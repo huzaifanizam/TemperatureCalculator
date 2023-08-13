7f82bd00c830d93fe0f5704e21e275f221670e5a
HUZAIFA NIZAM <121801411+huzaifanizam@users.noreply.github.com>
2023-08-13 22:12:08 +0500

    Temperature Calculator

---
+++ app.mjs
@@ -0,0 +1,18 @@
+let temp = prompt("Enter your room temp; " )
+
+if (temp > 30){
+    console.log("garmi hai");
+}  else if  (temp > 25){
+    console.log("Mosum theek hai");
+}  else if  (temp > 20){
+    console.log("use jacket when you ride a bike");
+}  else if  (temp <= 20){
+    console.log(" carry jacket");
+}  else if  (temp <= 10){
+    console.log(" its cold");
+} else if  (temp <= 0){
+    console.log(" avoid going outside");
+} 
+
+
+
